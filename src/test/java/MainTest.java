a5ca9a223dc7b25bc4e5aab82befa15314864abb
thanhlongphan <thanhlong.phan92@gmail.com>
2022-03-24 09:34:12 +0100

    jenkins-maven-test

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    @Test
+    void dummyTest() {
+
+    }
+
+    @Test
+    void shouldAddNumber() {
+        assertEquals(4,4);
+    }
+}
